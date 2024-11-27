8e4b37143498a91889412a8fd70d71c0f75ab746
Jacek Trossen <jacek.trossen@code-intelligence.com>
2024-11-27 10:41:46 +0100

    CI Fuzz applied

--- src/explore_me.cpp
+++ src/explore_me.cpp
@@ -9,16 +9,18 @@ int exploreMe(int a, int b, string c) {
     if (b >= 2000000) {
       if (b - a < 100000) {
         int n = 23;
-        n <<= 32;
+        n <<= a;
 
         char hello[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-        char world[20] = {'W', 'o', 'r', 'l', 'd', '!', '\0'};
-        if (c == "FUZZING") {
-          strcpy(world, "exampleWorkshop");
+        char world[8] = {'W', 'o', 'r', 'l', 'd', '!','!', '\0'};
+        if (c.find("FUZZING") != std::string::npos) {
+          strcpy(world, c.c_str());
         }
         else if(c == "seg-fault"){
-          char arr[5] = {'a', 'b', 'c', 'd', 'e'};
-          printf("Char at position %d: %c\n", (b-a)/100, arr[(b-a)/100]);
+          char arr[6] = {'a', 'b', 'c', 'd', 'e', '\0'};
+          int pos = (b-a)/5;
+          arr[pos] = 'n';
+          printf("Character %c written at position %d.\nNew string: %s\n", arr[pos], pos, arr);
         }
         printf("%s\n", hello);
         printf("%s\n", world);
